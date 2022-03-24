4f421b7653386244d0f4f38fb438e4a2235653de
natchanon krasin <knatchanon@netizen.co.th>
2022-03-24 09:28:37 +0000

    master

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Connect Success")
+const { Connection, Request } = require("tedious");
+
+// Create connection to database
+const config = {
+  authentication: {
+    options: {
+      userName: "supleman@hack2build", // update me
+      password: "Stcc1234" // update me
+    },
+    type: "default"
+  },
+  server: "hack2build.database.windows.net", // update me
+  options: {
+    database: "vrock_db", //update me
+    encrypt: true
+  }
+};
+
+
+
+const connection = new Connection(config);
+
+
+connection.connect(console.log("Connect Success"));
+
